c27c9b59e7c6798e7d714380d2d1cdbe3de60f3d
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-11-23 18:04:22 +0100

    Refactor CarParksOverview

--- app/src/main/java/com/pietervandewalle/androidapp/ui/carparks/CarParksOverview.kt
+++ app/src/main/java/com/pietervandewalle/androidapp/ui/carparks/CarParksOverview.kt
@@ -3,11 +3,13 @@ package com.pietervandewalle.androidapp.ui.carparks
 import android.text.format.DateUtils
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -54,72 +56,82 @@ fun CarParkList(modifier: Modifier = Modifier, carParks: List<CarPark>) {
 fun CarParkListItem(modifier: Modifier = Modifier, carPark: CarPark) {
     ListItem(
         shadowElevation = 2.dp,
-        modifier = modifier
-            .padding(5.dp),
-        headlineText = {
-            Text(carPark.name, style = MaterialTheme.typography.titleMedium)
-        },
-        supportingText = {
-            Column(
-                modifier = Modifier.padding(top = 10.dp),
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-            ) {
-                Text(
-                    carPark.description,
-                    style = MaterialTheme.typography.bodyMedium,
-                )
-
-                Text(
-                    "Laatste update: " + DateUtils.getRelativeTimeSpanString(carPark.lastUpdate.atZone(ZonedDateTime.now().zone).toInstant().toEpochMilli()),
-                    style = MaterialTheme.typography.bodySmall,
-                )
-            }
-        },
-        trailingContent = {
-            Card(
-                colors = CardDefaults.cardColors(containerColor = determineColor(carPark.isOpenNow, isAlmostFull = carPark.isAlmostFull, isFull = carPark.isFull)),
-            ) {
-                Column(modifier = Modifier.padding(8.dp).width(100.dp)) {
-                    if (carPark.isOpenNow) {
-                        Text(
-                            if (carPark.isFull) {
-                                stringResource(R.string.full)
-                            } else if (carPark.isAlmostFull) {
-                                stringResource(R.string.almost_full)
-                            } else {
-                                stringResource(
-                                    R.string.available_space,
-                                )
-                            },
-
-                            style = MaterialTheme.typography.bodyLarge,
-                            textAlign = TextAlign.Center,
-                        )
-                        if (carPark.availableCapacity != 0) {
-                            Text(
-                                carPark.availableCapacity.toString() + " plaatsen vrij",
-                                style = MaterialTheme.typography.bodySmall,
-                            )
-                        }
-                    } else {
-                        Text(
-                            if (carPark.isTemporaryClosed) "Tijdelijk gesloten" else "Gesloten",
-                            style = MaterialTheme.typography.bodyMedium,
-                        )
-                    }
-                }
-            }
-        },
+        modifier = modifier.padding(5.dp).height(120.dp),
+        headlineText = { CarParkName(carPark = carPark) },
+        supportingText = { CarParkDetails(carPark = carPark) },
+        trailingContent = { CarParkStatus(cardColor = determineStatusColor(carPark), carPark = carPark) },
     )
 }
 
 @Composable
-fun determineColor(isOpen: Boolean, isAlmostFull: Boolean, isFull: Boolean): Color {
-    if (!isOpen || isFull) {
+private fun CarParkName(carPark: CarPark) {
+    Text(carPark.name, style = MaterialTheme.typography.titleMedium)
+}
+
+@Composable
+private fun CarParkDetails(carPark: CarPark) {
+    Column(
+        modifier = Modifier.padding(top = 10.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+    ) {
+        Text(carPark.description, style = MaterialTheme.typography.bodyMedium)
+        Text(
+            "Laatste update: " + DateUtils.getRelativeTimeSpanString(
+                carPark.lastUpdate.atZone(ZonedDateTime.now().zone).toInstant().toEpochMilli(),
+            ),
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
+@Composable
+private fun CarParkStatus(cardColor: Color, carPark: CarPark) {
+    Card(colors = CardDefaults.cardColors(containerColor = cardColor), shape = RoundedCornerShape(4.dp)) {
+        Column(modifier = Modifier.padding(8.dp).width(100.dp)) {
+            if (carPark.isOpenNow) {
+                CarParkOpenStatus(carPark = carPark)
+            } else {
+                CarParkClosedStatus(carPark = carPark)
+            }
+        }
+    }
+}
+
+@Composable
+private fun CarParkOpenStatus(carPark: CarPark) {
+    Text(
+        when {
+            carPark.isFull -> stringResource(R.string.full)
+            carPark.isAlmostFull -> stringResource(R.string.almost_full)
+            else -> stringResource(R.string.available_space)
+        },
+        style = MaterialTheme.typography.bodyLarge,
+        textAlign = TextAlign.Center,
+    )
+
+    if (!carPark.isFull) {
+        Text(
+            "${carPark.availableCapacity} plaatsen vrij",
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
+@Composable
+private fun CarParkClosedStatus(carPark: CarPark) {
+    Text(
+        if (carPark.isTemporaryClosed) "Tijdelijk gesloten" else "Gesloten",
+        style = MaterialTheme.typography.bodyMedium,
+    )
+}
+
+@Composable
+fun determineStatusColor(carPark: CarPark): Color {
+    if (!carPark.isOpenNow || carPark.isFull) {
         return MaterialTheme.colorScheme.errorContainer
     }
 
-    if (isAlmostFull) {
+    if (carPark.isAlmostFull) {
         return Color.hsl(42f, 0.78f, 0.92f)
     }
     return Color.Green
