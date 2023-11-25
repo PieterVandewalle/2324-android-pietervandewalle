d5458aeff0093a42c96a66c5f6e171dd229b6b5b
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-11-25 19:44:55 +0100

    Add limit query param to api calls

--- app/src/main/java/com/pietervandewalle/androidapp/network/GhentApiService.kt
+++ app/src/main/java/com/pietervandewalle/androidapp/network/GhentApiService.kt
@@ -3,9 +3,9 @@ package com.pietervandewalle.androidapp.network
 import retrofit2.http.GET
 
 interface GhentApiService {
-    @GET("bezetting-parkeergarages-real-time/records")
+    @GET("bezetting-parkeergarages-real-time/records?limit=20")
     suspend fun getCarParks(): ApiResult<ApiCarPark>
 
-    @GET("recente-nieuwsberichten-van-stadgent/records")
+    @GET("recente-nieuwsberichten-van-stadgent/records?limit=20")
     suspend fun getArticles(): ApiResult<ApiArticle>
 }
